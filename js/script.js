8248455b69873322d0703881c82beafb4b47083b
Eriawyn <alabau.philippe@gmail.com>
2023-07-15 19:13:52 +0200

    Finalisation du projet

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-function scrollToForm() {
-  document.getElementById("form-contact").submit();
-}
-
+fetch("url", {
+  method: "POST",
+  body: JSON.stringify({
+      name:"",
+      contact:"",
+      other:""
+  }),
+  headers: {
+      "Content-type": "application/json; charset=UTF-8"
+  }
+}).then((res) => {
+  console.log(res);
+})
